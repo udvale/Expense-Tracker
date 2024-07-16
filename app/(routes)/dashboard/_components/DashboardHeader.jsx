1e54c505b5f55bf0df30e5029b99e8ff04563365
udvale <enkhtaiu@dickinson.edu>
2024-07-16 18:52:58 -0400

    Changes & :Reapply "updates"
    This reverts commit 1fa17c4d0fd1c95193e022d506de36ba196c7dc7.

---
+++ app/(routes)/dashboard/_components/DashboardHeader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function DashboardHeader() {
+  return (
+    <div className=" bg-customGray p-5 flex justify-between">
+      <div>{/* <SignOutButton redirectUrl="/" /> */}</div>
+    </div>
+  );
+}
+
+export default DashboardHeader;
---
+++ app/(routes)/dashboard/budgets/_components/BudgetItem.jsx
@@ -0,0 +1,60 @@
+import Link from "next/link";
+import React from "react";
+
+function BudgetItem({budget}) {
+  const calculateProgressPerc = () => {
+    // (spend/total)*100
+    const perc = (budget.totalSpend / budget.amount) * 100;
+    return perc > 100 ? 100 : perc.toFixed(2);
+  };
+  return (
+    <Link href={"/dashboard/expenses/" + budget?.id}>
+      <div
+        className="p-5 border rounded-lg
+   hover:shadow-md cursor-pointer h-[150px]  bg-silverCrest"
+      >
+        <div className="flex gap-2 items-center justify-between">
+          <div className="flex gap-2 items-center">
+            <h2
+              className="text-2xl p-3 px-4
+             bg-silverCrestDark rounded-full
+             "
+            >
+              {budget?.icon}
+            </h2>
+            <div>
+              <h2 className="font-bold">{budget.name}</h2>
+              <h2 className="text-sm text-gray-900">{budget.totalItem} Item</h2>
+            </div>
+          </div>
+          <h2 className="font-bold text-primary text-lg"> ${budget.amount}</h2>
+        </div>
+
+        <div className="mt-5 font-bold">
+          <div className="flex items-center justify-between mb-3">
+            <h2 className="text-xs text-slate-900">
+              ${budget.totalSpend ? budget.totalSpend : 0} Spend
+            </h2>
+            <h2 className="text-xs text-slate-900">
+              ${budget.amount - budget.totalSpend} Remaining
+            </h2>
+          </div>
+          <div
+            className="w-full
+             bg-silverCrestDark h-2 rounded-full"
+          >
+            <div
+              className="
+             bg-primary h-2 rounded-full"
+              style={{
+                width: `${calculateProgressPerc()}%`,
+              }}
+            ></div>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+}
+
+export default BudgetItem;
